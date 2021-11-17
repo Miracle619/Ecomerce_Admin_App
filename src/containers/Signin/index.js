c59de061a8a1c0eb8282ad66617c1d1ea4c50497
dipesh <merchant.dipesh@gmail.com>
2021-11-17 20:15:31 +0530

    Updated Sign in method

--- src/containers/Signin/index.js
+++ src/containers/Signin/index.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import Layout from "../../components/Layout";
 import { Container, Form, Button, Row, Col } from "react-bootstrap";
 import Input from "../../components/UI/Input";
-
+import   { useSelector,useDispatch } from 'react-redux'
+import { Redirect } from "react-router";
+// import {} from '../../actions'
 const Signin = (props) => {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const auth = useSelector(state => state.auth);
 
+  const dispatch = useDispatch();
+
+  const userLogin =(e) =>{
+      e.preventDefault()
+
+      const user = {
+        email,password
+      }
+      dispatch(login(user))
+  }
+
+    if (auth.authenticate){
+      return <Redirect to={`/`} /> 
+    }
 
   return (
     <Layout>
